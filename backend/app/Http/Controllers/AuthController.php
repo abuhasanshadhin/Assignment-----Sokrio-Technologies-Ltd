5732a02ed0d9b5ad89d16c84d56638f6dd41a59c
Abu Hasan Shadhin <abuhasanshadhin98@gmail.com>
2021-04-30 16:31:32 +0600

    Login functionalities added

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        $credentials = $request->only('username', 'password');
 
+        if ($token = $this->guard()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $this->guard()->factory()->getTTL() * 60,
+        ]);
+    }
+
+    public function guard()
+    {
+        return Auth::guard();
     }
 }
