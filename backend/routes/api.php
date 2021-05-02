20da4e6b5f4ce31a4c0bb800c968b0aea7e40c3a
Abu Hasan Shadhin <abuhasanshadhin98@gmail.com>
2021-05-02 19:55:31 +0600

    Protect unauthorized user access

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,16 +24,21 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/check-auth', [AuthController::class, 'checkAuth']);
     Route::get('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/branches', [BranchController::class, 'getBranches']);
-    Route::post('/branches', [BranchController::class, 'addBranch']);
-    Route::put('/branches/{id}', [BranchController::class, 'updateBranch']);
-    Route::delete('/branches/{id}', [BranchController::class, 'deleteBranch']);
+    Route::middleware('admin')->group(function () {
 
-    Route::get('/users', [UserController::class, 'getUsers']);
-    Route::post('/users', [UserController::class, 'addUser']);
-    Route::put('/users/{id}', [UserController::class, 'updateUser']);
-    Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
+        Route::get('/branches', [BranchController::class, 'getBranches']);
+        Route::post('/branches', [BranchController::class, 'addBranch']);
+        Route::put('/branches/{id}', [BranchController::class, 'updateBranch']);
+        Route::delete('/branches/{id}', [BranchController::class, 'deleteBranch']);
+
+        Route::get('/users', [UserController::class, 'getUsers']);
+        Route::post('/users', [UserController::class, 'addUser']);
+        Route::put('/users/{id}', [UserController::class, 'updateUser']);
+        Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
+
+    });
 
     Route::get('/attendance/receive', [AttendanceController::class, 'receiveAttendance']);
     Route::get('/will-check-in', [AttendanceController::class, 'willCheckIn']);
+
 });
