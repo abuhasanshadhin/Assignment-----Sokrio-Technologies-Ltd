e069118824eff010aca1f03e2adf2fcadbe51602
Abu Hasan Shadhin <abuhasanshadhin98@gmail.com>
2021-05-02 09:15:15 +0600

    Attendance migration file created

---
+++ backend/database/migrations/2021_05_01_145035_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->dateTime('check_in');
+            $table->dateTime('check_out')->nullable();
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
