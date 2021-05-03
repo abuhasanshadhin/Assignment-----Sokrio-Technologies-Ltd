5f30de5980bb95dbd1f39112d70b64452c151330
Abu Hasan Shadhin <abuhasanshadhin98@gmail.com>
2021-05-03 10:28:57 +0600

    get total duration of employee check-in and check-out

--- backend/app/Models/Attendance.php
+++ backend/app/Models/Attendance.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Attendance extends Model
 {
     use HasFactory;
 
     protected $fillable = ['employee_id', 'check_in', 'check_out'];
+
+    protected $appends = ['duration'];
+
+    public function getDurationAttribute()
+    {
+        $checkIn = new DateTime($this->check_in);
+
+        $checkOut = $this->check_out ?? date('Y-m-d H:i:s', strtotime('17:00'));
+        $checkOut = new DateTime($checkOut);
+
+        $diff = $checkIn->diff($checkOut);
+
+        return $diff->format('%H:%I:%S');
+    }
 }
